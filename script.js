20f2237d6d4ab429f825ce481f491023eb15065b
Vadim <vadim_boll@mail.ru>
2020-09-26 10:39:37 +0400

    calc programm part 1

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-//alert ("Hello");
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD","");
 
-let num = prompt ("Сколько вам лет?", "");
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
 
-if (num > 18) {
-    alert ("Добро пожаловать!");
-} else if (num == 18) {
-    alert ("Подожди секунду, покажи паспорт!");
-} else {
-    alert ("Уходи!");
-}
+let a1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let a2 = prompt("Во сколько обойдется?","");
+let a3 = prompt("Введите обязательную статью расходов в этом месяце","");
+let a4 = prompt("Во сколько обойдется?","");
 
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
+              
